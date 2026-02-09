279419886ff1b60a645ca46c90a461162dd2a306
Paul Kim <pawulkim@gmail.com>
2026-02-08 20:13:37 -0700

    HW1 Requirements

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.text({ type: "*/*" }));
 
@@ -8,7 +8,7 @@ app.post("/", (req, res) => {
   res.status(200).type("text/plain").send(req.body || "");
 });
 
-// Optional: so browser GET doesn't look scary
+
 app.get("/", (req, res) => {
   res.status(200).type("text/plain").send("Send a POST request to / to get an echo.");
 });
